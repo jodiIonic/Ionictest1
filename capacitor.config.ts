d026c2eca7ac7ef940722ae87542d6f4221c8d16
jodiIonic <jodi@ionic.io>
2022-09-07 10:56:54 -0400

    test2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.jodi',
   appName: 'photo-gallery',
   webDir: 'www',
   bundledWebRuntime: false
 };
 
 export default config;
+
+//making even more changes to add
+//something else to add
